8f50ace79c7717adb863e25efb6d1f9b9f460f22
proussel <pierre.roussel01@free.fr>
2022-08-11 19:31:27 +0200

    init: base lib and data fetch OK

--- app.js
+++ app.js
@@ -1,65 +1,79 @@
-import {h, component} from './lib.js';
+import { h, component } from "./lib.js";
 
 function loadData() {
-    // TODO: load from data.json
+  return fetch("./data.json")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 const button = component((props, children) => {
-    // TODO
+  // TODO
 });
 
 const taskItem = component((props, children, rerender) => {
-    function toggle(event) {
-        // TODO
-    }
+  function toggle(event) {
+    // TODO
+  }
 
-    function rename() {
-        // TODO
-    }
+  function rename() {
+    // TODO
+  }
 
-    const checkStatus = props.task.done ? {checked: ''} : {};
-    return h('div', {class: 'task-item'}, [
-        h('input', {type: 'checkbox', onclick: toggle, ...checkStatus}),
-        button({onclick: rename}, "rename"),
-        button({onclick: () => props.onTaskDeleted()}, "delete")
-    ]);
+  const checkStatus = props.task.done ? { checked: "" } : {};
+  return h("div", { class: "task-item" }, [
+    h("input", { type: "checkbox", onclick: toggle, ...checkStatus }),
+    button({ onclick: rename }, "rename"),
+    button({ onclick: () => props.onTaskDeleted() }, "delete"),
+  ]);
 });
 
 const taskList = component((props) => {
-    // TODO: loop on props.tasks to generate a list of taskItem
-    // call props.onTaskDeleted(task) when a task is deleted
+  // TODO: loop on props.tasks to generate a list of taskItem
+  // call props.onTaskDeleted(task) when a task is deleted
 });
 
 const addTaskForm = component((props) => {
-    function onsubmit(event) {
-        // TODO: call props.onTaskCreared() with a task object as argument
-    }
+  function onsubmit(event) {
+    // TODO: call props.onTaskCreared() with a task object as argument
+  }
 
-    return h('form', {onsubmit}, [
-        h('input', {type: 'text', placeholder: 'Task title', name: 'title'}),
-        button({type: 'submit'}, 'Add')
-    ]);
+  return h("form", { onsubmit }, [
+    h("input", { type: "text", placeholder: "Task title", name: "title" }),
+    button({ type: "submit" }, "Add"),
+  ]);
 });
 
 let tasks;
 const app = component((props, children, rerender) => {
-    if (tasks) {
-        return h('div', {id: 'app'}, [
-            taskList({tasks, onTaskDeleted: (task) => {
-                // TODO
-            }}),
-            addTaskForm({onTaskCreated: (task) => {
-                // TODO
-            }})
-        ]);
-    }
+  if (tasks) {
+    return h("div", { id: "app" }, [
+      taskList({
+        tasks,
+        onTaskDeleted: (task) => {
+          // TODO
+        },
+      }),
+      addTaskForm({
+        onTaskCreated: (task) => {
+          // TODO
+        },
+      }),
+    ]);
+  }
 
-    loadData().then((data) => {
-        tasks = data;
-        rerender();
-    });
+  loadData().then((data) => {
+    tasks = data;
+    console.log("data", data);
+    rerender();
+  });
 
-    return h('div', {class: 'loading'}, 'Loading...');
+  return h("div", { class: "loading" }, "Loading...");
 });
 
-document.addEventListener('DOMContentLoaded', () => document.body.appendChild(app()));
+document.addEventListener("DOMContentLoaded", () =>
+  document.body.appendChild(app())
+);
--- lib.js
+++ lib.js
@@ -1,1 +1,34 @@
-// TODO!
+export function h(tagName, attributes, children) {
+  const el = document.createElement(tagName);
+  for (let [key, value] of Object.entries(attributes)) {
+    if (key.match(/^on/)) {
+      el.addEventListener(key.substring(2).toLowerCase(), value);
+    } else {
+      el.setAttribute(key, value);
+    }
+  }
+  if (typeof children === "string") {
+    el.appendChild(document.createTextNode(children));
+  } else if (Array.isArray(children)) {
+    for (let child of children) {
+      el.appendChild(child);
+    }
+  } else if (children) {
+    el.appendChild(children);
+  }
+  return el;
+}
+
+export function component(renderFunc) {
+  return function (props, children) {
+    let el;
+    function rerender() {
+      let newEl = renderFunc(props, children, rerender);
+      el.replaceWith(newEl);
+      el = newEl;
+    }
+
+    el = renderFunc(props, children, rerender);
+    return el;
+  };
+}
